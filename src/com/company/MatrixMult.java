d47b88f9fe28968551c7fb82c914a6101f0d9074
jessqin <jessicaq319@gmail.com>
2016-12-21 14:23:33 -0500

    Initial commit

---
+++ src/com/company/MatrixMult.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by jq210 on 12/21/16.
+ */
+public class MatrixMult
+{
+    public static int[][] mult(int x[][], int y[][])
+    {
+        int product[][] = new int[x.length][y[0].length];
+
+        for (int i = 0; i < 1; i++)
+        {
+
+        }
+
+        return product;
+
+    }
+}
