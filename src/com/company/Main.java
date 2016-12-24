1858aa60193380953c543e59a8938bbdfe6d3dc7
Jessica Qin <jessicaq319@gmail.com>
2016-12-23 21:37:45 -0500

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,18 @@ public class Main {
 
     public static void main(String[] args)
     {
-	    int a [][] = new int[3][4];
-        int b [][] = new int[4][2];
+	    int a [][] = {{1,2,-2,0},{-3,4,7,2},{6,0,3,1}};
+        int b [][] = {{-1,3},{0,9},{1,-11},{4,-5}};
+
+        int ans [][] = MatrixMult.mult(a,b);
+        for (int row = 0; row<ans.length; row++)
+        {
+            for (int col = 0; col<ans[0].length; col++)
+            {
+                System.out.print(ans[row][col] + "\t");
+            }
+            System.out.println("");
+        }
 
     }
 }
--- src/com/company/MatrixMult.java
+++ src/com/company/MatrixMult.java
@@ -9,9 +9,15 @@ public class MatrixMult
     {
         int product[][] = new int[x.length][y[0].length];
 
-        for (int i = 0; i < 1; i++)
+        for (int i = 0; i < x.length; i++)
         {
-
+            for (int j = 0; j < y[0].length; j++)
+            {
+                for (int k = 0; k < x[0].length; k++)
+                {
+                    product[i][j] = product[i][j] + (x[i][k] * y[k][j]);
+                }
+            }
         }
 
         return product;
